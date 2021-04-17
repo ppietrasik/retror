e0685671b3b3b00ac063e3398d7c7ce93f1554bb
ppietrasik <18726317+ppietrasik@users.noreply.github.com>
2021-04-17 23:54:32 +0200

    add stimultus app setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import  "../stylesheets/application.scss";
+import "../controllers"
 
 Rails.start()
 ActiveStorage.start()
