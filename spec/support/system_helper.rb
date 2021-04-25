45d2006e57e668c106e8522ff74563ffef944beb
ppietrasik <18726317+ppietrasik@users.noreply.github.com>
2021-04-25 23:10:34 +0200

    remove custom precompile script

--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -12,36 +12,4 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by Capybara.javascript_driver
   end
-
-  # Asset precompilation
-  config.before(:suite) do
-    examples = RSpec.world.filtered_examples.values.flatten
-    has_no_system_tests = examples.none? { |example| example.metadata[:type] == :system }
-
-    if has_no_system_tests
-      $stdout.puts "\n🚀️️  No system test selected. Skip assets compilation.\n"
-      next
-    end
-
-    if Webpacker.dev_server.running?
-      $stdout.puts "\n⚙️  Webpack dev server is running! Skip assets compilation.\n"
-      next
-    else
-      $stdout.puts "\n🐢  Precompiling assets.\n"
-      original_stdout = $stdout.clone
-
-      start = Time.current
-      begin
-        # Silence Webpacker output
-        $stdout.reopen(File.new('/dev/null', 'w'))
-
-        require 'rake'
-        Rails.application.load_tasks
-        Rake::Task['webpacker:compile'].execute
-      ensure
-        $stdout.reopen(original_stdout)
-        $stdout.puts "Finished in #{(Time.current - start).round(2)} seconds"
-      end
-    end
-  end
 end
