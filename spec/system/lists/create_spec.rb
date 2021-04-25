326f5adde1864efb132a82f20b55102fe0b74714
ppietrasik <18726317+ppietrasik@users.noreply.github.com>
2021-04-25 22:58:45 +0200

    add synchronization test to create list system spec

--- spec/system/lists/create_spec.rb
+++ spec/system/lists/create_spec.rb
@@ -14,12 +14,30 @@ describe 'Board/Lists -> Create' do
     expect { press_create }.to change(List, :count).from(0).to(1)
   end
 
-  it 'renderes new list in the dom', :aggregate_failures do
-    expect(page).to have_no_selector('div[data-list-id]')
+  it 'renders new list in the dom', :aggregate_failures do
+    expect(page).to have_no_selector('div[data-list-id-value]')
 
     press_create
 
     created_list = List.last
-    expect(page).to have_selector("div[data-list-id='#{created_list.id}']")
+    expect(page).to have_selector("div[data-list-id-value='#{created_list.id}']")
+  end
+
+  context 'with second widow open' do
+    let(:second_window) { open_new_window }
+
+    it "renders new list in the second window's dom", :aggregate_failures do
+      within_window second_window do
+        visit board_path(board)
+        expect(page).to have_no_selector('div[data-list-id-value]')
+      end
+
+      press_create
+      created_list = List.last
+
+      within_window second_window do
+        expect(page).to have_selector("div[data-list-id-value='#{created_list.id}']")
+      end
+    end
   end
 end
