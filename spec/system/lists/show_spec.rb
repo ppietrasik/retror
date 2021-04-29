abb4ed8205d5758a03e2324e9cd7aa60bcb7207c
ppietrasik <18726317+ppietrasik@users.noreply.github.com>
2021-04-29 23:46:54 +0200

    add list order system spec

--- spec/system/lists/show_spec.rb
+++ spec/system/lists/show_spec.rb
@@ -3,27 +3,47 @@ require 'rails_helper'
 describe 'Board/Lists -> Show' do
   subject(:visit_page) { visit board_path(board) }
 
-  let(:board) { list.board }
-  let(:list) { create(:list, name: name) }
-
-  let(:name) { 'My List' }
+  let(:board) { create(:board) }
+  let!(:list_1) { create(:list, board: board, name: 'My List') }
 
   it 'displays existing list' do
     visit_page
 
-    expect(page).to have_selector("input[value='My List']")
+    expect(page).to have_selector("input[value='#{list_1.name}']")
   end
 
   context 'with multiple lists' do
-    let(:second_list_name) { 'My Second List' }
+    let!(:list_2) { create(:list, board: board, name: 'My Second List') }
+    let!(:list_3) { create(:list, board: board, name: 'My Third List') }
 
-    before { create(:list, board: board, name: second_list_name) }
+    before do
+      list_1.insert_at(1)
+      list_2.insert_at(0)
+      list_3.insert_at(2)
+    end
 
     it 'displays multiple existing lists', :aggregate_failures do
       visit_page
 
-      expect(page).to have_selector("input[value='My List']")
-      expect(page).to have_selector("input[value='My Second List']")
+      expect(page).to have_selector("input[value='#{list_1.name}']")
+      expect(page).to have_selector("input[value='#{list_2.name}']")
+      expect(page).to have_selector("input[value='#{list_3.name}']")
     end
+
+    it 'displays lists in a correct order', :aggregate_failures do
+      visit_page
+
+      list_at_pos_0 = find_list_by_index(0)
+      list_at_pos_1 = find_list_by_index(1)
+      list_at_pos_2 = find_list_by_index(2)
+
+      expect(list_at_pos_0).to have_selector("input[value='#{list_2.name}']")
+      expect(list_at_pos_1).to have_selector("input[value='#{list_1.name}']")
+      expect(list_at_pos_2).to have_selector("input[value='#{list_3.name}']")
+    end
+  end
+
+  def find_list_by_index(index)
+    all("div[data-controller='list']")[index]
   end
 end
